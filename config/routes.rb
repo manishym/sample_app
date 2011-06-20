2d1bc63d9794e1ab23777db24170993a2e594bbd
Manish M Yathnalli <manish.ym@gmail.com>
2011-06-20 21:54:11 +0530

    End of chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
+  
+   root :to => 'pages#home'
 
   get "pages/contact"
   
@@ -10,6 +14,9 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup', :to => 'users#new'
+  
+ 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "Should have home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+  
+  it "Should have contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+  it "Should have About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content =>"About")
+  end
+  it "Should have Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
+  it "Should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+end
