49ecc4186b891eb73587f635368bf8d2b4d555cc
Manish M Yathnalli <manish.ym@gmail.com>
2011-07-03 07:41:15 +0530

    Added sessions to manage users login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,13 +24,17 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   def has_password? (submitted_password)
     #stub, code will be added
-    encrypt_password == encrypt(submitted_password)
+    encrypted_password == encrypt(submitted_password)
   end
   
   def self.authenticate (email, submitted_password)
     user = User.find_by_email(email)
     return nil if user.nil?
     return user if user.has_password? (submitted_password) 
+  end
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
   end
   private
     def encrypt_password
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,16 @@ require 'rspec/rails'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  def test_sign_in (user)
+    controller.sign_in(user)
+  end
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button  
+  end
+    
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
