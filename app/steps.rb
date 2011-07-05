0f37e11150c999aac2b6bb8179d16b8b3f583deb
Manish M Yathnalli <manish.ym@gmail.com>
2011-07-05 19:07:47 +0530

    Added working pagination and tests: Added some new files which were not tracked

---
+++ app/steps.rb
@@ -0,0 +1,47 @@
+
+# add will_paginate gem 
+# use User.paginate(:page => params[:page]) instead of 
+# 
+# 
+# 
+
+
+
+
+
+
+
+
+
+
+=======================================================================
+# done
+
+
+=======================================================================
+# Step 2
+# install faker gem
+# create a rake task in lib/tasks folder: sample_data.rake
+=======================================================================
+# Step 1
+# write test to casue index to redirect for non signed in user in user_controlled_spec
+# make it pass by adding :index to before filter
+
+# write test to cause index pass to signed in user
+# make it pass by creating a template
+
+# make test to make sure index has right title.
+# make it pass
+
+
+# make few more users using Factory(:user, :email => "value")
+# make a test to make sure User.all.each has a <li> eliment
+# make it pass by creating @users variable in controller and
+# adding a li in loop for each user in @users
+
+# Integration test for layout
+# create a navigation test to test that Users link is present in
+# navigation and make it pass
+
+
+
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe "Users" do
+  describe "signup" do
+    describe "failure" do
+      it "should not make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Your name",    :with => ""
+          fill_in "Email",        :with => "" 
+          fill_in "Password",     :with => ""
+          fill_in "Confirmation", :with => ""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector("div#error_explanation")
+        end.should_not change(User, :count)
+      end
+    end
+    describe "success" do
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Your name",    :with => "Manish"
+          fill_in "Email",        :with => "man.ishym@gmail.com" 
+          fill_in "Password",     :with => "hangloose"
+          fill_in "Confirmation", :with => "hangloose"
+          click_button
+          response.should render_template('users/show')
+          response.should have_selector("div,flash.success",
+                                         :content => "Welcome")
+        end.should change(User, :count).by(1)
+      end
+    end
+  end
+
+  describe "sign in/out" do
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email,         :with => ""
+        fill_in :password,      :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")  
+      end
+    end
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        integration_sign_in(user)
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in 
+      end
+    end
+  end
+end
